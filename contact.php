b8dd72c327ac8831153ae7bd4c6857a5a70c4898
Chamika Shashipriya <chamikashashipriya3@gmail.com>
2025-12-02 17:19:10 +0530

    Update contact form shortcode in contact.php and remove unused JavaScript code from javascript.js

--- contact.php
+++ contact.php
@@ -41,7 +41,7 @@ get_header();
             <div class="col-md-8 col-md-offset-2">
                 <div class="ptc-form">
                     <?php
-                    echo do_shortcode( '[contact-form-7 id="fed605b" title="Contact from"]' ); // CF7 short code for contact form 1
+                    echo do_shortcode( '[contact-form-7 id="c88e8e7" title="C1"]' ); // CF7 short code for contact form 1
                     ?>
                 </div>
             </div>
